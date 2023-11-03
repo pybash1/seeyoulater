88beabcc2800f4ae07281fcc54dfb1833edf75e0
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-11-03 19:03:59 +0530

    feat: production build with working buttons

--- public/popup.js
+++ public/popup.js
@@ -6,6 +6,38 @@ function snoozeTab(time) {
   });
 }
 
+let msd = 1440 * 60000;
+
 document.getElementById("testing").onclick = function () {
   snoozeTab(20000);
+};
+document.getElementById("oneday").onclick = function () {
+  snoozeTab(msd);
+};
+document.getElementById("twoday").onclick = function () {
+  snoozeTab(2 * msd);
+};
+document.getElementById("threeday").onclick = function () {
+  snoozeTab(3 * msd);
+};
+document.getElementById("fourday").onclick = function () {
+  snoozeTab(4 * msd);
+};
+document.getElementById("fiveday").onclick = function () {
+  snoozeTab(5 * msd);
+};
+document.getElementById("sixday").onclick = function () {
+  snoozeTab(6 * msd);
+};
+document.getElementById("sevenday").onclick = function () {
+  snoozeTab(7 * msd);
+};
+document.getElementById("fifteenday").onclick = function () {
+  snoozeTab(15 * msd);
+};
+document.getElementById("twentyoneday").onclick = function () {
+  snoozeTab(21 * msd);
+};
+document.getElementById("thirtyday").onclick = function () {
+  snoozeTab(30 * msd);
 };
